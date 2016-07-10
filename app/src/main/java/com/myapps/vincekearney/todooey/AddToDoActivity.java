c4f9eec712c56a7feaafb18432abfb9aa57bac58
Vince Kearney <vincent.kearney@saltdna.com>
2016-07-10 19:44:25 +0100

    Added items to the xml for the layout and some stuff in the activity

--- app/src/main/java/com/myapps/vincekearney/todooey/AddToDoActivity.java
+++ app/src/main/java/com/myapps/vincekearney/todooey/AddToDoActivity.java
@@ -2,12 +2,25 @@ package com.myapps.vincekearney.todooey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 
 public class AddToDoActivity extends AppCompatActivity {
+
+    public static final String TASK_DESC = "task";
+
+    private EditText descView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+        descView = (EditText) findViewById(R.id.toDoText);
+    }
+
+    public void addToDoClicked(View view)
+    {
+
     }
 }
