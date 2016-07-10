640af32cc68557ea5f01a0e9df01ba18770325c8
Vince Kearney <vincent.kearney@saltdna.com>
2016-07-10 19:24:14 +0100

    List view added to the ToDoListActivity and the FAB starting the add one

--- app/src/main/java/com/myapps/vincekearney/todooey/ToDoListActivity.java
+++ app/src/main/java/com/myapps/vincekearney/todooey/ToDoListActivity.java
@@ -1,36 +1,43 @@
 package com.myapps.vincekearney.todooey;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
+//import android.view.WindowManager;
 
 public class ToDoListActivity extends AppCompatActivity
 {
+    private final int TODO_ADDED = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        // Here we are telling the app that we want to be full screen.
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Here we are telling the app that we want to be full screen - I.e. The menu bar at the top is gone (date and battery thang).
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_to_do_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if(fab != null)
+        if(fab != null) // Ensure the FAB has been created before adding a listener - Threw an error otherwise.
         {
             fab.setOnClickListener(new View.OnClickListener()
             {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    Intent addToDoIntent = new Intent(ToDoListActivity.this, AddToDoActivity.class);
+                    startActivityForResult(addToDoIntent,TODO_ADDED);
+                    // We are using the above so that the activity that we launch
+                    // within this result gets passed data back. We can use information like
+                    // this for certain things - Here we are using it to determine whether a To-Do
+                    // item was added or not.
                 }
             });
         }
