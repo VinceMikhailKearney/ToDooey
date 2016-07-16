9feaa17590c29564c2f1b64f97bcd87719811184
Vince Kearney <vincent.kearney@saltdna.com>
2016-07-16 21:11:26 +0100

    Cleaned and organised the DBManager - Should hopefully prevent any more fuck ups when messing around with the table and columns

--- app/src/main/java/com/myapps/vincekearney/todooey/DBManager.java
+++ app/src/main/java/com/myapps/vincekearney/todooey/DBManager.java
@@ -7,32 +7,40 @@ import android.util.Log;
 
 public class DBManager extends SQLiteOpenHelper
 {
+    /* ---- TAG and Helper strings ---- */
     private static final String TAG = "DBManager";
+    private static final String TYPE_TEXT = "TEXT";
+    private static final String TYPE_INTEGER = "INTEGER";
+    private static final String formatTextType = String.format(" %s, ",TYPE_TEXT);
+    private static final String formatTextTypeEnd = String.format(" %s",TYPE_TEXT);
+    private static final String formatIntegerType = String.format(" %s, ",TYPE_INTEGER);
+    /* ---- Database ---- */
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "ToDooey.db";
+    /* ---- Table and Columns (in order) ---- */
     public static final String TO_DO_ITEMS_TABlE = "ToDoItems";
     public static final String COLUMN_NAME_TODO_ID = "todoid";
     public static final String COLUMN_NAME_TODO_TEXT = "todotext";
     public static final String COLUMN_NAME_COMPLETED = "completed";
     public static final String COLUMN_NAME_DATE = "date";
-
+    /* ---- Create table SQL string ---- */
     private static final String CREATE_DATABASE =
                     "CREATE TABLE IF NOT EXISTS " + TO_DO_ITEMS_TABlE +"("
-                    + COLUMN_NAME_TODO_ID + " TEXT, "
-                    + COLUMN_NAME_TODO_TEXT + " TEXT, "
-                    + COLUMN_NAME_COMPLETED + " INTEGER, "
-                    + COLUMN_NAME_DATE + " TEXT" + ")";
+                    + COLUMN_NAME_TODO_ID + formatTextType
+                    + COLUMN_NAME_TODO_TEXT + formatTextType
+                    + COLUMN_NAME_COMPLETED + formatIntegerType
+                    + COLUMN_NAME_DATE + formatTextTypeEnd + ")";
 
     public DBManager(Context context)
     {
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
-        Log.i(TAG, "Just instantiating the DBManager.");
+        Log.i(TAG, "Setting up DBManager.");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        Log.i(TAG, "Creating the database - " + CREATE_DATABASE);
+        Log.i(TAG, "Creating the database: " + CREATE_DATABASE);
         db.execSQL(CREATE_DATABASE);
     }
 
@@ -44,5 +52,5 @@ public class DBManager extends SQLiteOpenHelper
         db.execSQL("DROP TABLE IF EXISTS " + TO_DO_ITEMS_TABlE);
         onCreate(db);
     }
-    //==============================END OF CLASS==============================
+    /* ===============END OF CLASS=============== */
 }
