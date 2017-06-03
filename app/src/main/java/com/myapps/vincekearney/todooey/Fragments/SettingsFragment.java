992470c5c303e61fdf2c374bf658c88e7ce3cbd5
Vince <vincent.kearney@saltdna.com>
2017-06-04 00:49:38 +0100

    If we turn off the parent setting then the children need to be reset :)

--- app/src/main/java/com/myapps/vincekearney/todooey/Fragments/SettingsFragment.java
+++ app/src/main/java/com/myapps/vincekearney/todooey/Fragments/SettingsFragment.java
@@ -1,17 +1,48 @@
 package com.myapps.vincekearney.todooey.Fragments;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.myapps.vincekearney.todooey.R;
+import com.myapps.vincekearney.todooey.ToDoApplication;
 
-public class SettingsFragment extends PreferenceFragment
-{
+import java.util.prefs.Preferences;
+
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "SettingsFragment";
+    private static final String HIDE_ITEMS = "pref_key_hide";
+    private static final String HIDE_ALL_ITEMS = "pref_key_hide_all";
+
+    CheckBoxPreference parentHideItems;
+    CheckBoxPreference hideAllItems;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.preferences);
+        parentHideItems = (CheckBoxPreference) findPreference(HIDE_ITEMS);
+        hideAllItems = (CheckBoxPreference) findPreference(HIDE_ALL_ITEMS);
+
+        parentHideItems.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Log.i(TAG,"We got a preference change");
+        switch (preference.getKey()) {
+            case HIDE_ITEMS:
+                Log.i(TAG, "Found the right pref");
+                if((boolean) newValue == false) {
+                    Log.i(TAG, "Are we even hitting this");
+                    PreferenceManager.getDefaultSharedPreferences(ToDoApplication.getAppContext()).edit().putBoolean(HIDE_ALL_ITEMS,false).commit();
+                    hideAllItems.setChecked(false);
+                }
+        }
+        return true;
     }
 }
